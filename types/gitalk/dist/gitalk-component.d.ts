475e40a26fbc5efd8ea2979f96dfe5d11bbbe553
Chen Junda <smallda@outlook.com>
2019-02-28 11:08:16 +0800

    add types

---
+++ types/gitalk/dist/gitalk-component.d.ts
@@ -0,0 +1,8 @@
+import * as React from "react";
+import Gitalk from "..";
+
+declare class GitalkComponent extends React.Component<{ options: Gitalk.GitalkOptions }> {
+
+}
+
+export default GitalkComponent;
